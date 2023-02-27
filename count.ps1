3abacf71daf3376fd68ceb4ce4ea179a0df4cff2
kmarten-cargas <kmarten@cargas.com>
2023-02-27 15:10:52 -0500

    Introduce count script used for getting record counts from tables

---
+++ count.ps1
@@ -0,0 +1,32 @@
+﻿param(
+    [Parameter(Position=0)][alias("t")][String]$table,
+    [Parameter(ValueFromRemainingArguments=$true,Position=1)][alias("c")][String[]]$columns,
+    [alias("d")][String]$database
+)
+
+$text = ""
+
+if ($database)
+{
+    $text = "use $database `n"
+}
+
+$text = $text + "select"
+
+if ($columns)
+{
+    $text = $text + " $($columns -join ", "),"
+}
+
+
+$text = "$text count(*) `nfrom $table"
+
+if ($columns)
+{
+    $text = $text + "`ngroup by $($columns -join ", ")"
+}
+
+$text = $text + "`nhaving count(*) > 1"
+
+Write-Host $text
+Set-Clipboard $text
