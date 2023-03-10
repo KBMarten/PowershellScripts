e851e9f6068e7eda29e9d43a4a8f55d9b555c88d
kmarten-cargas <kmarten@cargas.com>
2023-03-10 09:49:31 -0500

    Create test script even if the SQL file doesn't start with "dbo."

--- TestConversionScripts.ps1
+++ TestConversionScripts.ps1
@@ -2,13 +2,13 @@
     $null = md ConversionScriptsTest
 }
 
-foreach($file in Get-ChildItem -Path '.\dbo.*.sql' -Recurse -Depth 3) {
+foreach($file in Get-ChildItem -Path '.\*.sql' -Recurse -Depth 3) {
     write-host "Writing $($file.Name) to ConversionScriptsTest folder... " -NoNewline
     ((Get-Content -path $file.FullName -Raw) -replace 'ConversionScripts\b','ConversionScriptsTest') | Set-Content -Path ".\ConversionScriptsTest\$($file.Name)"
     write-host "Done!"
 }
 
-foreach($file in Get-ChildItem -Path '.\ConversionScriptsTest\dbo.*.sql' -Recurse -Depth 3) {
+foreach($file in Get-ChildItem -Path '.\ConversionScriptsTest\*.sql' -Recurse -Depth 3) {
     ((Get-Content -path $file.FullName -Raw) -replace 'CargasEnergy\b','CargasEnergyTest') | Set-Content -Path ".\ConversionScriptsTest\$($file.Name)"
 }
 
